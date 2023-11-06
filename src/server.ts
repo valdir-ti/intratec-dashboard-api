7580951a59ef849cc4e68abbb27a469759a5a5f5
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-06 17:35:49 -0300

    feat: configure server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,25 @@
-function foo (a: number, b: number) {
-    return a + b
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+
+import routes from './routes/authRoutes'
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+try {   
+    mongoose.connect(process.env.MONGODB_LINK || "")
+    console.log('MongoDB connected');    
+} catch (error) {
+    console.log('MongoDB error connection => ', error);    
 }
+
+app.use("/api", routes)
+
+app.listen(PORT, () => {
+    console.log(`App listenning at ${PORT}`);    
+})
