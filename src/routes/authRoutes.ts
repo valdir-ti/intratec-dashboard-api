f9173221a77818810ab1c2ad78d165f9b52f1c8f
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-07 15:41:25 -0300

    feat: auth process

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import { SignUp } from "../controllers/SignUp"
 import { Login } from "../controllers/Login"
+import { Auth } from "../controllers/Auth"
 
 const router = Router()
 
 router.post("/signup", SignUp)
 router.post("/login", Login)
+router.post("/auth", Auth)
 
 export default router
