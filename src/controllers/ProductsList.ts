6796a6523c71bcd92ed6e7c8bc9bc92f7a1d18ea
Valdir Silva <valdir.ti@gmail.com>
2023-11-22 21:55:34 -0300

    feat: create product list controlller and route

---
+++ src/controllers/ProductsList.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import User from '../models/Product'
+
+export const ProductsList = async (req: Request, res: Response) => {
+    const productsList = await User.find().select('title description image price stock isActive createdAt').exec()
+
+    try {
+        return res.status(200).json({
+            message: "Products get successfully",
+            data: productsList
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({
+            message: error
+        })
+    }
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,10 +4,12 @@ import { SignUp } from "../controllers/SignUp"
 import { Login } from "../controllers/Login"
 import { Auth } from "../controllers/Auth"
 import { UsersList } from '../controllers/UsersList'
+import { ProductsList } from '../controllers/ProductsList'
 
 const router = Router()
 
 router.get("/users", UsersList)
+router.get("/products", ProductsList)
 router.post("/signup", SignUp)
 router.post("/login", Login)
 router.post("/auth", Auth)
