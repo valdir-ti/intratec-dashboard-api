0b1c2725709f83440696ea36efe2a10f7fcf42c1
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-24 11:42:47 -0300

    feat: update product field return

--- src/controllers/ProductsList.ts
+++ src/controllers/ProductsList.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import User from '../models/Product'
 
 export const ProductsList = async (req: Request, res: Response) => {
-    const productsList = await User.find().select('title description image price stock isActive createdAt').exec()
+    const productsList = await User.find().select('title description price stock image size category isActive createdAt').exec()
 
     try {
         return res.status(200).json({
