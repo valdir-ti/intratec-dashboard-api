6179acf53ae620aa9ca8309fa2f3816213fd0031
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-27 18:00:56 -0300

    feat: add mongo client

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,32 @@
 import express, { Response } from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
+import { MongoClient } from './database/mongo'
 import 'dotenv/config'
 
 import routes from './routes/authRoutes'
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const main = async () => {
+	const app = express()
+	const PORT = process.env.PORT || 5000
 
-app.use(cors())
-app.use(express.json())
+	app.use(cors())
+	app.use(express.json())
 
-try {
-	mongoose.connect(process.env.MONGODB_LINK || '')
-	console.log('MongoDB connected')
-} catch (error) {
-	console.log('MongoDB error connection => ', error)
+	try {
+		await MongoClient.connect()
+		console.log('MongoDB connected')
+	} catch (error) {
+		console.log('MongoDB error connection => ', error)
+	}
+
+	app.use('/api', routes)
+	app.use('/', (_, res: Response) => {
+		res.status(200).json({ message: 'server is running' })
+	})
+
+	app.listen(PORT, () => {
+		console.log(`App listenning at ${PORT}`)
+	})
 }
 
-app.use('/api', routes)
-app.use('/', (_, res: Response) => {
-	res.status(200).json({ message: 'server is running' })
-})
-
-app.listen(PORT, () => {
-	console.log(`App listenning at ${PORT}`)
-})
-
-export default app
+main()
