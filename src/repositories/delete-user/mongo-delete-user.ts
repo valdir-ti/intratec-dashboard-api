098932ee5877dc9f1aaac78ebd19d24f9b3d31be
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-28 18:06:26 -0300

    feat: update return fields on delete user

--- src/repositories/delete-user/mongo-delete-user.ts
+++ src/repositories/delete-user/mongo-delete-user.ts
@@ -5,7 +5,9 @@ import { IDeleteUserRepository } from '../../controllers/delete-user/protocols'
 export class MongoDeleteUserRepository implements IDeleteUserRepository {
 	async deleteUser(id: string): Promise<IUser> {
 		const filter = { _id: id }
-		const userDeleted = await User.findByIdAndDelete(filter)
+		const userDeleted = (await User.findByIdAndDelete(filter).select(
+			'name email image isAdmin isActive createdAt',
+		)) as IUser
 
 		if (!userDeleted) throw new Error('user not deleted')
 
