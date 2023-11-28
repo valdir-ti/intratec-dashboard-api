4bf6ea40a166a02b530185718ad38c807bdc282d
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-28 16:15:21 -0300

    refact: validate user data as function

--- src/controllers/create-user/create-user.ts
+++ src/controllers/create-user/create-user.ts
@@ -1,5 +1,4 @@
-import User from '../../models/mongo/User'
-import isEmail from 'validator/lib/isEmail'
+import { validateUserData } from '../../utils/validate-user-data'
 import { IUser } from '../../models/interfaces/IUser'
 import { HttpRequest, HttpResponse } from '../protocols'
 import {
@@ -7,66 +6,7 @@ import {
 	ICreateUserController,
 	ICreateUserRepository,
 } from './protocols'
-
-const validateUserData = async (params: CreateUserParams) => {
-	const { name, email, password } = params
-
-	if (!name || !email || !password) {
-		return {
-			statusCode: 400,
-			body: 'Name, email and password are required',
-		}
-	}
-
-	if (name.trim().length === 0) {
-		return {
-			statusCode: 400,
-			body: 'Please enter a name',
-		}
-	}
-
-	if (email.trim().length === 0) {
-		return {
-			statusCode: 400,
-			body: 'Please enter an email',
-		}
-	}
-
-	if (!isEmail) {
-		return {
-			statusCode: 400,
-			body: 'Please enter a valid email',
-		}
-	}
-
-	if (password.trim().length === 0) {
-		return {
-			statusCode: 400,
-			body: 'Please enter a password',
-		}
-	}
-
-	if (password.trim().length <= 5) {
-		return {
-			statusCode: 400,
-			body: 'Password must have more than 5 characters',
-		}
-	}
-
-	const existingUser = await User.findOne({ email }).exec()
-
-	if (existingUser) {
-		return {
-			statusCode: 400,
-			body: 'User already exists',
-		}
-	}
-
-	return {
-		statusCode: 200,
-		body: 'isValid ok',
-	}
-}
+import User from '../../models/mongo/User'
 
 export class CreateUserController implements ICreateUserController {
 	constructor(private readonly createUserRepository: ICreateUserRepository) {}
@@ -82,7 +22,18 @@ export class CreateUserController implements ICreateUserController {
 				body: 'Please specify a body',
 			}
 		}
+
 		const isValid = await validateUserData(body)
+
+		const { email } = body
+		const existingUser = await User.findOne({ email }).exec()
+
+		if (existingUser) {
+			return {
+				statusCode: 400,
+				body: 'User already exists',
+			}
+		}
 
 		if (isValid.statusCode === 200) {
 			try {
