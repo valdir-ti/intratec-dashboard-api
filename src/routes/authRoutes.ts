b325c970d91f71e5470e3f83a71e476cfea6d96c
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-11-28 17:38:10 -0300

    feat: update user

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -10,9 +10,25 @@ import { GetUsersController } from '../controllers/get-user/get-users'
 import { MongoGetUsersRepository } from '../repositories/get-users/mongo-get-users'
 import { CreateUserController } from '../controllers/create-user/create-user'
 import { MongoCreateUserRepository } from '../repositories/create-user/mongo-create-user'
+import { MongoUpdateUserRepository } from '../repositories/update-user/mongo-update-user'
+import { UpdateUserController } from '../controllers/update-user/update-user'
 
 const router = Router()
 
+router.get('/users/:id', UserSingle)
+router.patch('/users/:id', async (req, res) => {
+	const mongoUpdateUserRepository = new MongoUpdateUserRepository()
+	const updateUserController = new UpdateUserController(
+		mongoUpdateUserRepository,
+	)
+
+	const { body, statusCode } = await updateUserController.handle({
+		body: req.body,
+		params: req.params,
+	})
+
+	res.status(statusCode).json(body)
+})
 router.get('/users', async (req, res) => {
 	const mongoGetUsersRepository = new MongoGetUsersRepository()
 	const getUsersController = new GetUsersController(mongoGetUsersRepository)
@@ -33,8 +49,6 @@ router.post('/users', async (req, res) => {
 
 	res.status(statusCode).json(body)
 })
-
-router.get('/users/:id', UserSingle)
 
 router.get('/products', ProductsList)
 router.get('/products/:id', ProductSingle)
