ccbf54d41d2f60249ce0ffe21e295c0e9b97da85
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-12-28 10:16:20 -0300

    feat: change return type on delete todo

--- src/repositories/todos/delete-todo/mongo-delete-todo.ts
+++ src/repositories/todos/delete-todo/mongo-delete-todo.ts
@@ -5,7 +5,7 @@ import Todo from "../../../models/mongo/Todo";
 export class MongoDeleteTodoRepository implements IDeleteTodoRepository {
     async deleteTodo(id: string): Promise<ITodo> {
         const filter = { _id: id }
-        const todoDeleted = await Todo.findByIdAndDelete(filter).select('description done createdAt')
+        const todoDeleted = (await Todo.findByIdAndDelete(filter).select('description done createdAt')) as ITodo
 
         if(!todoDeleted) throw new Error('todo not deleted')
 
