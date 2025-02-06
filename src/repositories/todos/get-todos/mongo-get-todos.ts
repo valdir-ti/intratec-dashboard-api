9e428be86ed4a4fcefcf5a2107e0d4760425a336
Valdir Silva <valdir.ti@gmail.com>
2025-02-06 17:13:50 -0300

    feat: filter todo by title or description

--- src/repositories/todos/get-todos/mongo-get-todos.ts
+++ src/repositories/todos/get-todos/mongo-get-todos.ts
@@ -8,26 +8,28 @@ import { ITodoResponse } from '../../../models/interfaces/ITodo'
 
 export class MongoGetTodosRepository implements IGetTodosRepository {
 	async getTodos(params: GetTodosParams): Promise<ITodoResponse> {
-		const q = params.q
+		const q = params?.q ? params?.q.trim() : ''
 		const page = params.page
         const order = !params.order || params.order === 'desc' ? -1 : 1
-
-        const regex = new RegExp(q, 'i')
-		const count = await Todo.find({
-			description: { $regex: regex },
-		}).countDocuments({})
-        const totalDone = await Todo.find({
-			done: true,
-		}).countDocuments({})
-        const totalOpen = await Todo.find({
-			done: false,
-		}).countDocuments({})
-		const data = await Todo.find({ description: { $regex: regex } })
-			.limit(TODOS_PER_PAGE)
-			.skip(TODOS_PER_PAGE * (parseInt(page) - 1))
-			.select('title description done createdAt')
-            .sort({ createdAt: order })
-            .exec()
+        const filters = q
+        ? {
+            $or: [
+                { description: { $regex: new RegExp(q, 'i') } },
+                { title: { $regex: new RegExp(q, 'i') } }
+            ]
+        }
+        : {};
+        const [count, totalDone, totalOpen, data] = await Promise.all([
+            Todo.countDocuments(filters),
+            Todo.countDocuments({ done: true }),
+            Todo.countDocuments({ done: false }),
+            Todo.find(filters)
+                .limit(TODOS_PER_PAGE)
+                .skip(TODOS_PER_PAGE * (parseInt(page) - 1))
+                .select('title description done createdAt')
+                .sort({ createdAt: order })
+                .exec()
+        ]);
 		return { count, totalDone, totalOpen, data }
 	}
 }
